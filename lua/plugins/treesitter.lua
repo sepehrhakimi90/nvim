a09f8c538d9a34fbc6de750a245d8c64183e6e07
sepehrhakimi90 <sepehrhakimi90@gmail.com>
2024-04-21 14:36:12 +0330

    reformat files and add golang configuration

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,13 +1,14 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function()
-    local configs = require("nvim-treesitter.configs")
-    configs.setup({
-      ensure_installed = { "c", "lua", "vim", "vimdoc", "javascript", "html", "go", "bash" },
-      sync_install = false,
-      highlight = { enable = true },
-      indent = { enable = true },  
-    })
-  end
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local configs = require("nvim-treesitter.configs")
+		configs.setup({
+			--ensure_installed = { "c", "lua", "vim", "vimdoc", "javascript", "html", "go", "bash" },
+			auto_install = true,
+			sync_install = false,
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
 }
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -5,3 +5,9 @@ vim.cmd("set shiftwidth=2")
 vim.cmd("set number")
 vim.cmd("set relativenumber")
 vim.g.mapleader = " "
+
+vim.keymap.set("n", "<C-t>n", ":tabnew<cr>", {})
+vim.keymap.set("n", "<C-t>c", ":tabc<cr>", {})
+vim.keymap.set("n", "<C-t>l", "gt", {})
+vim.keymap.set("n", "<C-t>h", "gT", {})
+vim.keymap.set("n", "<C-t>o", ":tabo<cr>", {})
