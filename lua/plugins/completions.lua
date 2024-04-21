d5ca582020d5b413f09377cc53c5b1c539342d5d
sepehrhakimi90 <sepehrhakimi90@gmail.com>
2024-04-21 16:03:27 +0330

    comment config

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -39,6 +39,8 @@ return {
 					{ name = "buffer" },
 				}),
 			})
+			local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+			cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done({ map_char = { tex = "" } }))
 		end,
 	},
 }
