9e39b9a936d01bab1a5200db64da37518dac9b0a
sepehrhakimi90 <sepehrhakimi90@gmail.com>
2024-04-27 11:58:24 +0330

    add git staff
    install some git modules

---
+++ lua/plugins/git-staff.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require("gitsigns").setup()
+      vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
+    end,
+  },
+  {
+    "kdheepak/lazygit.nvim",
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    keys = {
+      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+    },
+  },
+}
