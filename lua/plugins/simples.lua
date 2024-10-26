f49384892f8dafb41892a56c1b1204f04b8c06f3
nil <s.hakimi@agah.com>
2024-10-26 10:45:20 +0330

    tmux tab move

--- lua/plugins/simples.lua
+++ lua/plugins/simples.lua
@@ -1,4 +1,23 @@
 return {
-	"nvim-lua/plenary.nvim", -- lua functions that many plugins use
-	--"christoomey/vim-tmux-navigator", -- tmux & split window navigation
+  {"nvim-lua/plenary.nvim",},         -- lua functions that many plugins use
+  {
+    {
+  "christoomey/vim-tmux-navigator",
+  cmd = {
+    "TmuxNavigateLeft",
+    "TmuxNavigateDown",
+    "TmuxNavigateUp",
+    "TmuxNavigateRight",
+    "TmuxNavigatePrevious",
+  },
+  keys = {
+    { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+    { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+    { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+    { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+    { "<c-:>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+  },
 }
+  }
+}
+
