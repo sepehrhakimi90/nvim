d988f273b221375513f2312038b089f0ffc87c39
nil <s.hakimi@agah.com>
2024-11-02 10:05:57 +0330

    add todo plugin

--- lua/plugins/simples.lua
+++ lua/plugins/simples.lua
@@ -1,5 +1,5 @@
 return {
-  { "nvim-lua/plenary.nvim", }, -- lua functions that many plugins use
+  { "nvim-lua/plenary.nvim" }, -- lua functions that many plugins use
   {
     {
       "christoomey/vim-tmux-navigator",
@@ -17,25 +17,30 @@ return {
         { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
         { "<c-:>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
       },
-    }
+    },
   },
   {
-    'kevinhwang91/nvim-ufo',
-    dependencies = { 'kevinhwang91/promise-async' },
+    "kevinhwang91/nvim-ufo",
+    dependencies = { "kevinhwang91/promise-async" },
     opts = {
-      filetype_exclude = { 'help', 'alpha', 'dashboard', 'neo-tree', 'Trouble', 'lazy', 'mason' },
+      filetype_exclude = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy", "mason" },
     },
     config = function(_, opts)
-      vim.api.nvim_create_autocmd('FileType', {
-        group = vim.api.nvim_create_augroup('local_detach_ufo', { clear = true }),
+      vim.api.nvim_create_autocmd("FileType", {
+        group = vim.api.nvim_create_augroup("local_detach_ufo", { clear = true }),
         pattern = opts.filetype_exclude,
         callback = function()
-          require('ufo').detach()
+          require("ufo").detach()
         end,
       })
 
       vim.opt.foldlevelstart = 99
-      require('ufo').setup(opts)
+      require("ufo").setup(opts)
     end,
+  },
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {},
   },
 }
