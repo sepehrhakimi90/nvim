8e2268bdf2fbcc0e714b79d994f7a5be7e82729e
nil <s.hakimi@agah.com>
2024-11-05 10:54:19 +0330

    some improvment and spellings

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -32,15 +32,12 @@ return {
       keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
       keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
       keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
-      keymap.set(
-        "n",
-        "<leader>fc",
-        "<cmd>Telescope grep_string<cr>",
-        { desc = "Find string under cursor in cwd" }
-      )
+      keymap.set("n", "<leader>fc", "<cmd>Telescope registers<cr>", { desc = "Find in all registers" })
+      keymap.set("n", "<leader>fb", "<cmd>Telescope current_buffer_fuzzy_find<cr>", { desc = "Find current buffer" })
+      keymap.set("n", "<leader>fc", "<cmd>Telescopegrep_string<cr>", { desc = "Find string under cursor in cwd" })
     end,
   },
-  {
+
     "nvim-telescope/telescope-ui-select.nvim",
     config = function()
       require("telescope").setup({
